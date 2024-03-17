8ecd8e78ce672f3f40e2561eb681b4e1bfc1f35e
gybran nauval <nauvalgybran@gmail.com>
2024-03-17 16:58:58 +0700

    create page edit user

---
+++ lib/screen/edit_user_screnn.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class EditUser extends StatefulWidget {
+  const EditUser({super.key});
+
+  @override
+  State<EditUser> createState() => _EditUserState();
+}
+
+class _EditUserState extends State<EditUser> {
+  TextEditingController fullname = TextEditingController();
+  TextEditingController username = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController pass = TextEditingController();
+  GlobalKey<FormState> keyForm = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit User'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: Form(
+            key: keyForm,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: fullname,
+                  validator: (val) {
+                    return val!.isEmpty ? "Tidak boleh kosong" : null;
+                  },
+                  decoration: InputDecoration(
+                      labelText: "fullname",
+                      hintText: "fullname",
+                      border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 1, color: Colors.blue.shade300),
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  controller: username,
+                  validator: (val) {
+                    return val!.isEmpty ? "Tidak boleh kosong" : null;
+                  },
+                  decoration: InputDecoration(
+                      labelText: "username",
+                      hintText: "username",
+                      border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 1, color: Colors.blue.shade300),
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  controller: email,
+                  validator: (val) {
+                    return val!.isEmpty ? "Tidak boleh kosong" : null;
+                  },
+                  decoration: InputDecoration(
+                      labelText: "Email",
+                      hintText: "Email",
+                      border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 1, color: Colors.blue.shade300),
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  controller: pass,
+                  validator: (val) {
+                    return val!.isEmpty ? "Tidak boleh kosong" : null;
+                  },
+                  decoration: InputDecoration(
+                      labelText: "password",
+                      hintText: "password",
+                      border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 1, color: Colors.blue.shade300),
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  onPressed: () {
+                    if (keyForm.currentState?.validate() == true) {
+                      // tambahPegawai();
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text("silahkan isi data terlebih dahulu")));
+                    }
+                  },
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  height: 45,
+                  child: const Text("Submit"),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
