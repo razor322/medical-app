4eda9c233dd858211f40b1287dd431f0f0b3a170
gybran nauval <nauvalgybran@gmail.com>
2024-03-18 12:17:07 +0700

    add const

--- lib/const.dart
+++ lib/const.dart
@@ -1,1 +1,6 @@
-const url = '';
+import 'package:flutter/material.dart';
+
+const url = '10.127.215.156';
+const Color color1 = Color(0x008DDA);
+const Color color2 = Color(0x41C9E2);
+const Color color3 = Color(0xACE2E1);
